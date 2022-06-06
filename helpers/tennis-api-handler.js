f365f2281f8d8f0f885ece2c32c538422cb0636d
Colin <colin.dugeai@gmail.com>
2022-06-06 14:45:35 +0200

    First results of the api working
    We can get results for the desired timeslot

--- helpers/tennis-api-handler.js
+++ helpers/tennis-api-handler.js
@@ -1,3 +1,5 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 class TennisApiHandler {
 
     static coating_all= ["96","2095","94","1324","2016","92"]
@@ -22,6 +24,39 @@ class TennisApiHandler {
             selCoating: coating,
             selInOut: inOut
         }
+    }
+
+    static async postData(postData){
+
+        // ## Building the data to post (application/x-www-form-urlencoded format)
+        let params = new URLSearchParams();
+
+        // Attributes that contains array
+        let arrayAttributes = ['selCoating', 'selInOut'];
+        for (let attribute in postData) {
+            let dataValue = postData[attribute]
+
+            // If the attribute is an array
+            if (arrayAttributes.includes(attribute)) {
+                // Add each element of this array in the url
+                dataValue.forEach(e=> {
+                    params.append(attribute, e);
+                })
+            } else {
+                // Else, simply add the element in the url
+                params.append(attribute, dataValue)
+            }
+        }
+
+        // ## Posting the data
+        const response = await fetch(this.baseUrl, {
+            method: 'POST',
+            body: params.toString(),
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+
+        const data = await response.json();
+        return data;
     }
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const yaml = require('./helpers/yaml-handler')
+const tennisParis = require('./helpers/tennis-api-handler')
 
 
 
-let doc = yaml.load('watch.yml')
-console.log(doc.watch.unique[1]);
+const CONFIG = {
+}
+
+
+
+async function main() {
+
+    // Load yaml config file
+    let doc = yaml.load('watch.yml')
+
+    // Work on this timeslot
+    let schedule1 = doc.watch.unique[1];
+    console.log(schedule1);
+
+    // Get data to post for this timeslot
+    let postData = tennisParis.buildDataToPost(schedule1.time_start, schedule1.time_end, schedule1.date);
+    console.log(postData)
+
+    // Obtain the matching courts for this timeslot
+    let matchingCourts = await tennisParis.postData(postData);
+    console.log(matchingCourts)
+
+}
+
+
+
+main();
